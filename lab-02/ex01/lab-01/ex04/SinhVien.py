917b6df71f77008607424d4048ed46c61dd8cf19
tientai0612 <letaitai123456@gmail.com>
2024-04-08 14:40:12 +0700

    [add] lab-01 ex04 and .gitignore

---
+++ lab-02/ex01/lab-01/ex04/SinhVien.py
@@ -0,0 +1,8 @@
+class SinhVien:
+    def __init__(self, id, name, sex, major, diemTB):
+        self._id = id
+        self. name = name
+        self._sex = sex
+        self._major = major
+        self. diemTB = diemTB
+        self._hocLuc = ""
